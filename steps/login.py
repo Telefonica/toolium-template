c1d21e637a272dea1d367de4026d8cd54a31c438
Rubén González Alonso <ruben.gonzalezalonso@telefonica.com>
2026-02-13 09:55:52 +0100

    feat: configure ruff and format files

--- steps/login.py
+++ steps/login.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-u"""
+"""
 Copyright 2015 Telefónica Investigación y Desarrollo, S.A.U.
 This file is part of Toolium.
 
@@ -16,28 +15,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from behave import given, when, then
+from behave import given, then, when
 
 from pageobjects.login import LoginPageObject
 
 
 @given('the home page is open')
-def step_impl(context):
+def home_page_is_open(context):
     context.current_page = LoginPageObject()
     context.current_page.open()
 
 
 @when('the user logs in with username "{username}" and password "{password}"')
-def step_impl(context, username, password):
+def user_logs_in(context, username, password):
     user = {'username': username, 'password': password}
     context.current_page = context.current_page.login(user)
 
 
 @when('the user logs out')
-def step_impl(context):
+def user_logs_out(context):
     context.current_page = context.current_page.logout()
 
 
 @then('the message "{message}" is shown')
-def step_impl(context, message):
+def message_is_shown(context, message):
     assert message in context.current_page.message.get_message()
